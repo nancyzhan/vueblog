28fcdf545e9b1fcec1e1f6e9fb89d4c15e8607c0
nancyzhan <1020449768@qq.com>
2020-08-29 23:09:05 +0800

    ok

--- src/main/java/com/wqzhan/shiro/JwtFilter.java
+++ src/main/java/com/wqzhan/shiro/JwtFilter.java
@@ -1,8 +1,6 @@
 package com.wqzhan.shiro;
 
-import cn.hutool.http.server.HttpServerRequest;
 import cn.hutool.json.JSONUtil;
-import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.wqzhan.common.lang.Result;
 import com.wqzhan.util.JwtUtils;
 import io.jsonwebtoken.Claims;
@@ -13,6 +11,7 @@ import org.apache.shiro.web.filter.authc.AuthenticatingFilter;
 import org.apache.shiro.web.util.WebUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.ServletRequest;
@@ -23,50 +22,62 @@ import java.io.IOException;
 
 @Component
 public class JwtFilter extends AuthenticatingFilter {
+
     @Autowired
     JwtUtils jwtUtils;
+
     @Override
     protected AuthenticationToken createToken(ServletRequest servletRequest, ServletResponse servletResponse) throws Exception {
-        HttpServletRequest request=(HttpServletRequest) servletRequest;
-        String jwt=request.getHeader("Authorazation");
-        if(StringUtils.isEmpty(jwt)){
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        String jwt = request.getHeader("Authorization");
+        if(StringUtils.isEmpty(jwt)) {
             return null;
         }
+
         return new JwtToken(jwt);
     }
 
     @Override
     protected boolean onAccessDenied(ServletRequest servletRequest, ServletResponse servletResponse) throws Exception {
-        HttpServletRequest request=(HttpServletRequest) servletRequest;
-        String jwt=request.getHeader("Authorazation");
-        if(StringUtils.isEmpty(jwt)){
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        String jwt = request.getHeader("Authorization");
+        if(StringUtils.isEmpty(jwt)) {
             return true;
-        }else{
+        } else {
+
             // 校验jwt
-            Claims claims=jwtUtils.getClaimByToken(jwt);
-            if (claims==null ||jwtUtils.isTokenExpired(claims.getExpiration())){
+            Claims claim = jwtUtils.getClaimByToken(jwt);
+            if(claim == null || jwtUtils.isTokenExpired(claim.getExpiration())) {
                 throw new ExpiredCredentialsException("token已失效，请重新登录");
             }
-            //执行登录
-            return executeLogin(servletRequest,servletResponse);
+
+            // 执行登录
+            return executeLogin(servletRequest, servletResponse);
         }
     }
 
     @Override
     protected boolean onLoginFailure(AuthenticationToken token, AuthenticationException e, ServletRequest request, ServletResponse response) {
-        HttpServletResponse httpServletResponse=(HttpServletResponse) response;
-        Throwable throwable=e.getCause()==null?e:e.getCause();
-        Result result=Result.fail(throwable.getMessage());
-        String json= JSONUtil.toJsonStr(result);
+
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+
+        Throwable throwable = e.getCause() == null ? e : e.getCause();
+        Result result = Result.fail(throwable.getMessage());
+        String json = JSONUtil.toJsonStr(result);
+
         try {
             httpServletResponse.getWriter().print(json);
-        } catch (IOException ex) {
+        } catch (IOException ioException) {
+
         }
         return false;
     }
 
     @Override
     protected boolean preHandle(ServletRequest request, ServletResponse response) throws Exception {
+
         HttpServletRequest httpServletRequest = WebUtils.toHttp(request);
         HttpServletResponse httpServletResponse = WebUtils.toHttp(response);
         httpServletResponse.setHeader("Access-control-Allow-Origin", httpServletRequest.getHeader("Origin"));
@@ -81,3 +92,4 @@ public class JwtFilter extends AuthenticatingFilter {
         return super.preHandle(request, response);
     }
 }
+
